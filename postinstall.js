b3919785c0359759af816baf59cb8857946833b7
Romain Berger <romain@romainberger.com>
2019-02-28 14:20:45 +0100

    Just a test, will remove soon

---
+++ postinstall.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// I don't think this is against the terms of use of npm
+// but if it is let me know and I'll remove this!
+// Also if everybody think this is annoying and I suck
+// I'll remove it too, please let me know here:
+// https://github.com/romainberger/react-portal-tooltip/issues
+
+try {
+  console.log("\n\x1b[36mThanks for using react-portal-tooltip!\x1b[0m Do you listen to metal? Check this out http://bit.ly/2C79W4T\n")
+}
+catch(e) {}
